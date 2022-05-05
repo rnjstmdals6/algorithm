55c1225c88442243bfbc20e290c3242c6b834094
스피드권승민 <rnjstmdals6@naver.com>
2022-05-05 18:01:48 +0900

    [Gold V] Title: 뒤집기 3, Time: 148 ms, Memory: 113764 KB -BaekjoonHub

--- 백준/Gold/1464. 뒤집기 3/뒤집기 3.py
+++ 백준/Gold/1464. 뒤집기 3/뒤집기 3.py
@@ -5,14 +5,12 @@ input = sys.stdin.readline
 
 S = list(input().rstrip())
 
-temp = []
+temp = deque()
 temp.append(S[0])
 
 for i in range(1, len(S)):
     if temp[0] >= S[i]:
-        temp.reverse()
-        temp.append(S[i])
-        temp.reverse()
+        temp.appendleft(S[i])
     else:
         temp.append(S[i])
 
