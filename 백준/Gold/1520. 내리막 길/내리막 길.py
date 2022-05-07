e474ff820a66934037ae64bf313bf31ce124d6d5
스피드권승민 <rnjstmdals6@naver.com>
2022-05-07 22:16:43 +0900

    [Gold IV] Title: 내리막 길, Time: 216 ms, Memory: 148828 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,24 +1,24 @@
 import sys
-
 input = sys.stdin.readline
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+
+N, M = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+dp = [[-1] * M for _ in range(N)]
+
 
 def dfs(x, y):
-    if x == m-1 and y == n-1:
+    if x == N - 1 and y == M - 1:
         return 1
-    if c[x][y] != -1:
-        return c[x][y]
-    c[x][y] = 0
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < m and 0 <= ny < n:
-            if a[nx][ny] < a[x][y]:
-                c[x][y] += dfs(nx, ny)
-    return c[x][y]
+    if dp[x][y] != -1:
+        return dp[x][y]
 
-m, n = map(int, input().split())
-a = [list(map(int, input().split())) for _ in range(m)]
-c = [[-1]*n for _ in range(m)]
+    dp[x][y] = 0
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < N and 0 <= ny < M and graph[x][y] > graph[nx][ny]:
+            dp[x][y] += dfs(nx, ny)
+    return dp[x][y]
+
+
 print(dfs(0, 0))
