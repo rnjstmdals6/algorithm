9bb15e38aed31a59e495db30e27fba732938c554
Marine <rnjstmdals6@naver.com>
2022-08-14 19:07:21 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    s = list(s.lower())
+    
+    if s[0].isalpha():
+        s[0] = s[0].upper()
+    
+    for i in range(len(s) - 1):
+        if s[i] == ' ' and s[i + 1].isalpha():
+            s[i + 1] = s[i + 1].upper()
+    return "".join(s)
